cb8ef775bd235ecdb559f03b09b9a8dfcc791c84
juniorecheverri <juniorecheverri9@gmail.com>
2022-08-19 17:41:57 -0500

    se agrego la caperta dto a model

---
+++ nodejs/model/dto/card.dto.js
@@ -0,0 +1,30 @@
+//packages
+
+const mongoose = require("mongoose")
+ 
+/** using schema */
+const schema = require("../schemas/card.schema")
+
+schema.statics = {
+    create: function(data, cb){
+        let doc = new this(data)
+        doc.save(cb)
+    },
+
+    getAll: function(query, cb){
+       this.find(query, cb) 
+    },
+
+    getByFilter: function(query, cb){
+        this.find(query, cb) 
+     },
+    update: function(query,data,cb){
+        this.finOneAndUpdate(query, {$set: data},{new:true, cb})
+    },
+    delete: function(query,cb){
+        this.findOneAndDelete(query)
+    }
+}
+
+const dto = mongoose.model("coll_card", schema)
+module.exports = dto
